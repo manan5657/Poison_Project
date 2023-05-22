3b112c1537976876443aa0e407f34c970b0a8f2c
Manan Bhasin <mananbhasin5657@gmail.com>
2023-05-22 09:07:33 +0530

    Update j.js

--- j.js
+++ j.js
@@ -1,1 +1,8 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+    var welcomePlugin = document.getElementById("welcome-plugin");
+    var closeBtn = document.getElementById("close-btn");
+  
+    closeBtn.addEventListener("click", function() {
+      welcomePlugin.style.display = "none";
+    });
+  });
